856eb9dc3c74bd76d309ee511a210c122023c37f
Anirban Acharya <acharya.anirban9@gmail.com>
2020-05-31 14:46:53 +0530

    refactor

--- src/main/java/com/incubyte/app/StringCalculator.java
+++ src/main/java/com/incubyte/app/StringCalculator.java
@@ -3,10 +3,16 @@ package com.incubyte.app;
 public class StringCalculator {
 
   public int add(String numbers) {
-    if(numbers.isEmpty())
+    if (numbers.isEmpty())
       return 0;
-    if(numbers.length()==1)
-      return Integer.parseInt(numbers);
+    if (numbers.length() == 1) {
+      int result = 0;
+      try {
+        result = Integer.parseInt(numbers);
+      } catch (Exception e) {
+      }
+      return result;
+    }
     return 0;
   }
 }

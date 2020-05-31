42b57c968b214bd06e83d751a9edbd3afde5f89e
Anirban Acharya <acharya.anirban9@gmail.com>
2020-05-31 17:10:56 +0530

    refactor to support multiple negative numbers with any delimeters

--- src/main/java/com/incubyte/app/StringCalculator.java
+++ src/main/java/com/incubyte/app/StringCalculator.java
@@ -20,22 +20,56 @@ public class StringCalculator {
       throw new NegativeNumberException("negatives not allowed");
     }
     int sum = 0;
-    numbers = checkAndSanitizeDelimeterDefinition(numbers);
-    int[] nums = mapStringToArrayOfNumbers(numbers);
+    String delim = getDelimiter(numbers);
+    numbers = checkAndSanitizeDelimiterDefinition(numbers);
+    int[] nums = mapStringToArrayOfNumbers(numbers, delim);
     for (int number : nums)
       sum += number;
     return sum;
   }
 
-  private int[] mapStringToArrayOfNumbers(String numbers) {
-    String pattern = "\\D";
-    return Arrays.stream(numbers.split(pattern)).mapToInt(Integer::parseInt).toArray();
+  private int[] mapStringToArrayOfNumbers(String numbers, String delim)
+      throws NegativeNumberException {
+    String pattern = ",|\n";
+    if (delim != null && !delim.isEmpty())
+      pattern += "|" + delim;
+    int[] negatives = Arrays.stream(numbers.split(pattern)).mapToInt(Integer::parseInt)
+        .filter((e) -> e < 0).toArray();
+    if (negatives.length > 0) {
+      String errorMessage =
+          "negatives not allowed: " + integerArrayToStringRepresentation(negatives);
+      throw new NegativeNumberException(errorMessage);
+    }
+    return Arrays.stream(numbers.split(pattern)).mapToInt(Integer::parseInt).filter((e) -> e >= 0)
+        .toArray();
   }
 
-  private String checkAndSanitizeDelimeterDefinition(String numbers) {
+  private String checkAndSanitizeDelimiterDefinition(String numbers) {
     if (numbers.substring(0, 3).matches("//(\\D){1,1}")) {
       return numbers.split("\n")[1];
     }
     return numbers;
   }
+
+  private String getDelimiter(String numbers) {
+    String delim = "";
+    if (numbers.substring(0, 3).matches("//(\\D){1,1}")) {
+      delim = numbers.split("\n")[0].split("//")[1];
+    }
+    return delim;
+  }
+
+  private String integerArrayToStringRepresentation(int[] arr) {
+    StringBuilder build = new StringBuilder();
+    build.append('[');
+    for (int i = 0; i < arr.length; i++) {
+      build.append(arr[i]);
+      if (i < arr.length - 1)
+        build.append(',');
+    }
+    build.append(']');
+
+    return build.toString();
+  }
+
 }
